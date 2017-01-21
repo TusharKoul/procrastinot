bd0bdeca3fe64ae9aa2129afc061af12dbefb80e
dianlin <dil048@ucsd.edu>
2017-01-21 15:11:05 -0800

    Pie chart graph

---
+++ Downloads/procrastinot-master/src/Graph.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { VictoryBar } from 'victory';
+import {_victoryCore} from 'victory'
+
+export default class Graph extends Component {
+  render() {
+    return (
+
+      <VictoryBar width={500} labels = {"asjhdfguadf" }/>
+    );
+  }
+}
