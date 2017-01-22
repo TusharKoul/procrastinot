27fa2e6dc26069bb3213a783368a75dc3e2f4fde
tusharkoul <tusharkoul@gmail.com>
2017-01-22 08:58:29 -0800

    adding custom styling to leader cell

--- src/Leaderboard/LeaderCell.js
+++ src/Leaderboard/LeaderCell.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 
 export default class LeaderCell extends Component {
     // const myWidth = this.props.width;
-    render(){
+    render() {
+
+        const myStyle = {
+            width : this.props.width
+        };
+
         return (
-        <div className="leaderboard-cell" style={{width : 320}}>
+        <div className="leaderboard-cell" style={myStyle}>
             <div style={{display: 'inline-block', opacity: 0.8}}>
                 <div className="leaderboard-cell-name"> {this.props.name} </div>
                 <div> saved {this.props.timeSaved} this week </div>
--- src/Leaderboard/Leaderboard.js
+++ src/Leaderboard/Leaderboard.js
@@ -9,10 +9,10 @@ export default class Leaderboard extends Component {
                     <h4 className="card-header--title">Leaderboard</h4>
                 </div>
                 <div className="leaderboard-list">
-                    <LeaderCell name="Friend 1" icon="&#128526;" timeSaved='2hr 30min' width={330}/>
-                    <LeaderCell name="Friend 2" icon="&#128549;" timeSaved='2hr' width={270}/>
-                    <LeaderCell name="Friend 3" icon="&#128528;" timeSaved='1hr' width={200}/>
-                    <LeaderCell name="Friend 4" icon="&#x1f60a;" timeSaved='15min' width={190}/>
+                    <LeaderCell name="John" icon="&#128526;" timeSaved='2hr 30min' width={330}/>
+                    <LeaderCell name="Malcom" icon="&#x1f60a;" timeSaved='2hr' width={270}/>
+                    <LeaderCell name="Judy" icon="&#128528;" timeSaved='1hr' width={200}/>
+                    <LeaderCell name="Trisha" icon="&#128549;" timeSaved='15min' width={190}/>
                 </div>
             </div>
         );
